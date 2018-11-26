6dff5c63e99706bdf7712a1b53e20372a382c928
Mario <mariob360@gmail.com>
2018-11-25 21:24:49 -0800

    React router for dynamic component switching

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -9,5 +9,21 @@ import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
-render(<Main/>, document.getElementById('root'));
+// Import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+
+// Dynamic component switching bases on URL path
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
