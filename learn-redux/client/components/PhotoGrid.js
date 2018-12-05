32536be37ca7cbc330c713425dd6015d3910c147
Mario <mariob360@gmail.com>
2018-12-04 16:12:22 -0800

    Use redux store within containers

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        "Grid Component"
+          {/* {JSON.stringify(this.props.posts, null, ' ')} */}
+          {this.props.posts.map((post, i) => 
+            (
+            <Photo {...this.props} key={i} i={i} post={post}/>
+            )
+          )}
       </div>
     )
   }
