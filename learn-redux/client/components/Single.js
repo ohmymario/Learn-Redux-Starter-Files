bf44865eb5b6644a0d4d3cb55a2bce47e1e419d5
Mario <mariob360@gmail.com>
2018-12-06 17:10:03 -0800

    Pass in comments into Comments component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -5,19 +5,23 @@ import Comments from './Comments';
 const Single = React.createClass({
   render() {
 
+    // ID for Post
+    const { postId } = this.props.params
+
     // Post Index 
     const i = this.props.posts
-    .findIndex((post) => (
-      post.code === this.props.params.postId
-    ))
+    .findIndex((post) => (post.code === postId))
     
     // Post Information
     const post = this.props.posts[i];
 
+    // Post Comments
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props}/>
-        <Comments/>
+        <Comments postComments={postComments}/>
       </div>
     )
   }
